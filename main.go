cfef66261b7af5d211c647e7508ccb45aa04110b
Vaclav Samec <vaclav.samec@gmail.com>
2019-07-17 15:58:36 +0700

    added comments for exercise 01

--- main.go
+++ main.go
@@ -1,8 +1,33 @@
 package main
 
-import "fmt"
+//
+// GameData analyzer
+//
+// create program that open game data file 'data/GameData.json.gz', parse and analyze the content
+// the program should print:
+//
+// 1) compressed file size in bytes
+// 2) uncompressed size in bytes
+// 3) number of game data tables
+// 4) number of string values
+// 5) number of numerical values (Note: numerical value can be 1.0, 5 or "10" or '5.45')
+// 6) number of null values
+//
+//
+// example output:
+//
+//
+// Compressed size: 1234
+// Uncompressed size: 1234
+// Tables: 1234
+// Strings values: 1234
+// Numeric values: 1234
+// Null values: 1234
+//
+
 
 func main() {
 
-	fmt.Println("Hello world")
+	//! TODO: exercise 01
+
 }
