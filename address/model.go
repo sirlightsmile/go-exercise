338cf017ff1100f4bbe698c8c9c5af8118b3efe9
Sarunkorn Kanokaphiwat <sarunkorn@trueaxiongames.com>
2019-07-31 15:44:28 +0700

    update model

--- address/model.go
+++ address/model.go
@@ -1,16 +1,41 @@
 package address
 
 type Address struct {
-	SubDistrict string
-	District    string
-	Province    string
-	Zipcodes    string
+	SubDistrict District
+	District    Amphur
+	Province    Province
+	ZipCodes    ZipCode
+}
+
+type Amphur struct {
+	AmphurID      int
+	AmphurCode    string
+	AmphurName    string
+	AmphurNameEng string
+	ProvinceID    int
+	GeoID         int
+}
+
+type District struct {
+	DistrictID      int
+	DistrictCode    string
+	DistrictName    string
+	DistrictNameEng string
+	AmphurID        int
+	ProvinceID      int
+	GeoID           int
 }
 
 type Province struct {
-	ProvinceID      uint
-	ProvinceCode    uint
+	ProvinceID      int
+	ProvinceCode    int
 	ProvinceName    string
 	ProvinceNameEng string
-	GeoID           uint
+	GeoID           int
+}
+
+type ZipCode struct {
+	ID           int
+	DistrictCode int
+	ZipCode      string
 }
