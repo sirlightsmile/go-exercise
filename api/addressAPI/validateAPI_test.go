2197fc865008f4822c0909ae70473a8dc96f7cf1
Sarunkorn Kanokaphiwat <sarunkorn@trueaxiongames.com>
2019-08-09 12:47:50 +0700

    validation test WIP

---
+++ api/addressAPI/validateAPI_test.go
@@ -0,0 +1,40 @@
+package addressAPI
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"smile/repository"
+	"strings"
+	"testing"
+)
+
+func TestValidaionAPI(t *testing.T) {
+	absPath, _ := filepath.Abs("../data/th_address.db")
+	db, _ := repository.ConnectSqlDB(absPath)
+
+	req, err := http.NewRequest("GET", "/validate", strings.NewReader(`{"SubDistrict":{"ID":1,"Code":"100101","Name":"พระบรมมหาราชวัง","NameEng":"Phra Borom Maha Ratchawang","GeoID":2,"AmphurID":1,"ProvinceID":1},"District":{"ID":1,"Code":"1001","Name":"เขตพระนคร   ","NameEng":"Khet Phra Nakhon","GeoID":2,"ProvinceID":1},"Province":{"ID":1,"Code":"10","Name":"กรุงเทพมหานคร   ","NameEng":"Bangkok","GeoID":2},"ZipCode":{"ID":1,"SubDistrict":"100101","ZipCode":"10200"}}`))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	validateApi := &Validate{}
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		validateApi.GetHandler(db, w, r)
+	})
+
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := `true` + "\n" //encode auto new line. response need \n
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
