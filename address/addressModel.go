77d9093e2647d3bf1458f33321a8349f8c57f90f
Sarunkorn Kanokaphiwat <sarunkorn@trueaxiongames.com>
2019-08-13 13:29:37 +0700

    clean unuse model

--- address/addressModel.go
+++ address/addressModel.go
@@ -2,14 +2,6 @@ package address
 
 import "smile/repository"
 
-type AddressInterface interface {
-	NewAddress(subDistrictName string, districtName string, provinceName string, zipcode string) Address
-	GetProvinces() ([]Province, error)
-	GetDistrictsByProvince(provinceName string) ([]Amphur, error)
-	GetZipcodesByDistrict(districtName string) ([]ZipCode, error)
-	Validate(address Address) bool
-}
-
 type AddressManager struct {
 	repo repository.QueryInterface
 }
