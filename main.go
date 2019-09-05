f723935248978057fad028802a70c1e496ba7d6c
Vaclav Samec <vaclav.samec@gmail.com>
2019-09-05 16:21:43 +0700

    exercise 07

--- main.go
+++ main.go
@@ -1,37 +1,59 @@
 package main
 
 //
-// Goroutines #3
+// Implement string HashSet
+//
+// - string HashSet is a Set that accepts string values
+// - don't use map[] for implementation
+// - HashSet is usually implemented as an array of buckets where each bucket contains a single linked list
+// - https://en.wikipedia.org/wiki/Hash_table
+//
+// - initialize HashSet with bucket size = 1
+// - implement all methods from the interface
+// - all tests should pass
+//
+// - here's an example flow of adding an element:
+// - 1) set.Add(<item>)
+// - 2) calculate hash of the <item>
+// - 3) use hash to get index of the bucket
+// - 4) if the bucket is empty add your element as a first node of single linked list
+// - 5) if the bucket is not empty (collision) append the element to the end of linked list
 //
 
-//
-// Implement fixed size blocking queue, the queue must be thread-safe
-//
-// blocking queue is a queue that works same as normal queue but blocks enqueue/dequeue until the operation is available
-//
-// - to create a queue use function NewQueue(capacity int) *Queue, the capacity represents maximum elements of the queue
-// - the queue should work with multiple goroutines, for example one routine enqueuing, two other routines are dequeueing
-//
+type HashSet interface {
 
+	// Adds the specified element to a set, returns true if the element is added to the HashSet; false if the element is already present.
+	Add(item string) bool
 
-type Queue interface {
+	// Removes the specified element from a HashSet, returns true if the element is successfully found and removed; otherwise false.
+	Remove(item string) bool
 
-	// enqueue element into queue, if the size of the queue is >= capacity, the function blocks until the size is < capacity
-	Enqueue(interface{})
+	// Determines whether a HashSet object contains the specified element.
+	Contains(item string) bool
 
-	// dequeue element from queue, if the size is 0, the function blocks until the size > 0
-	Dequeue() interface{}
+	// Allow using custom hash function.
+	SetHashFunction(func(string) int)
 
-	// returns current size of the queue
-	Size() int
+	// Gets the number of elements that are contained in a HashSet.
+	Count() int
 
-	// returns capacity of the queue
-	Capacity() int
+	// Gets the number of buckets in a HashSet.
+	BucketCount() int
 
-	// clear the queue, size will be 0
+	// Removes all elements from a HashSet.
 	Clear()
+
+	// Internal method that will resize and re-hash all elements if the load factor > 1 (load factor = number of elements / number of buckets)
+	// resize operation will double the bucket size and rehash all elements
+	// this should be called automatically when adding a new element
+	resize()
 }
 
+const initialBucketSize = 1
+
+func NewHashSet() HashSet {
+	return nil
+}
 
 func main() {
 }
--- main_test.go
+++ main_test.go
@@ -1,95 +1,95 @@
 package main
 
 import (
-	"sync"
+	"gotest.tools/assert"
+	"math/rand"
+	"strconv"
 	"testing"
 )
 
-// test 1 producer - 1 consumer
-func TestQueue(t *testing.T) {
-	q := NewQueue(10)
+func TestSet_Add(t *testing.T) {
 
-	iteration := 10000000
-	wait := sync.WaitGroup{}
-	wait.Add(2)
+	set := NewHashSet()
 
-	go func() {
-		defer wait.Done()
-		for i:=0; i<iteration; i++ {
-			q.Enqueue(i)
+	// use crappy hash function to always collide, resize should be triggered when count == power^2
+	set.SetHashFunction(func(s string) int {
+		return 1
+	})
+
+	for i := 1; i<=128; i++ {
+		item := strconv.Itoa(i)
+		assert.Assert(t, !set.Contains(item), "Item %s can't be inside!", item)
+
+		set.Add(item)
+		assert.Assert(t, set.Contains(item), "Item %s must be inside!", item)
+		assert.Assert(t, set.Count() == i, "Invalid set count: %d", set.Count())
+
+		isPow := (set.Count() & (set.Count() - 1)) == 0
+
+		// resize should be triggered when count == power^2
+		if isPow {
+			assert.Assert(t, set.BucketCount() == set.Count(), "Invalid bucket count: %d, expected: %d", set.BucketCount(), set.Count())
 		}
-	}()
-
-	go func() {
-		defer wait.Done()
-		for i:=0; i<iteration; i++ {
-			q.Dequeue()
-		}
-	}()
-
-	wait.Wait()
+	}
 }
 
-// test 2 producers - 1 consumer
-func TestQueue2(t *testing.T) {
-	q := NewQueue(10)
+func TestSet_Dup(t *testing.T) {
 
-	iteration := 10000000
-	wait := sync.WaitGroup{}
-	wait.Add(3)
+	set := NewHashSet()
+	set.SetHashFunction(func(s string) int {
+		return rand.Int()
+	})
 
-	go func() {
-		defer wait.Done()
-		for i:=0; i<iteration; i++ {
-			q.Enqueue(i)
-		}
-	}()
-
-	go func() {
-		defer wait.Done()
-		for i:=0; i<iteration/2; i++ {
-			q.Dequeue()
-		}
-	}()
-
-	go func() {
-		defer wait.Done()
-		for i:=0; i<iteration/2; i++ {
-			q.Dequeue()
-		}
-	}()
-
-	wait.Wait()
+	assert.Assert(t, set.Add("item"), "Item should be added!")
+	assert.Assert(t, !set.Add("item"), "Item cannot be added!")
+	assert.Assert(t, set.Count() == 1, "Item count must be 1!")
 }
 
-// test 1 producer - 2 consumers
-func TestQueue3(t *testing.T) {
-	q := NewQueue(10)
+func TestSet_Remove(t *testing.T) {
 
-	iteration := 10000000
-	wait := sync.WaitGroup{}
-	wait.Add(3)
+	set := NewHashSet()
+	set.SetHashFunction(func(s string) int {
+		return rand.Int()
+	})
 
-	go func() {
-		defer wait.Done()
-		for i:=0; i<iteration/2; i++ {
-			q.Enqueue(i)
-		}
-	}()
+	counter := 0
 
-	go func() {
-		defer wait.Done()
-		for i:=0; i<iteration/2; i++ {
-			q.Enqueue(i)
-		}
-	}()
+	for i := 1; i<=128; i++ {
+		item := strconv.Itoa(i)
+		assert.Assert(t, !set.Contains(item), "Item %s can't be inside!", item)
 
-	go func() {
-		defer wait.Done()
-		for i:=0; i<iteration; i++ {
-			q.Dequeue()
-		}
-	}()
+		set.Add(item)
+		counter ++
 
-	wait.Wait()
+		assert.Assert(t, set.Contains(item), "Item %s must be inside!", item)
+		assert.Assert(t, set.Count() == counter, "Invalid set count: %d, expected: %d", set.Count(), counter)
+
+		removed := set.Remove(item)
+		counter --
+		assert.Assert(t, removed, "Item %s must be removed!", item)
+		assert.Assert(t, set.Count() == counter, "Invalid set count: %d, expected: %d", set.Count(), counter)
+
+		assert.Assert(t, !set.Remove(item), "Item cannot be removed twice!")
+		assert.Assert(t, set.Count() == counter, "Invalid set count: %d, expected: %d", set.Count(), counter)
+	}
+
+	assert.Assert(t, set.Count() == 0, "All elements must be 0")
+}
+
+func TestSet_Clear(t *testing.T) {
+
+	set := NewHashSet()
+	set.SetHashFunction(func(s string) int {
+		return rand.Int()
+	})
+
+	for i:=0; i<128; i++ {
+		set.Add(strconv.Itoa(i))
+	}
+
+	assert.Assert(t, set.Count() == 128, "Invalid set count: %d", set.Count())
+
+	set.Clear()
+
+	assert.Assert(t, set.Count() == 0, "Invalid set count: %d", set.Count())
 }
